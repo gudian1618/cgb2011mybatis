cd451e50f5380bde27c0ff9748b8bf17f44810a5
gudian1618 <1761844211@qq.com>
2021-02-18 19:38:20 +0800

    Initial commit

---
+++ src/main/java/com/github/gudian1618/cgb2011mybatis/TestMybatis01.java
@@ -0,0 +1,32 @@
+package com.github.gudian1618.cgb2011mybatis;
+
+import org.junit.Test;
+
+/**
+ * @author gudian1618
+ * @version v1.0
+ * @date 2021/2/16 9:07 上午
+ * 编写mybatis快速入门程序
+ */
+
+public class TestMybatis01 {
+
+    /**
+     * 查询emp表中的所以员工信息
+     */
+    @Test
+    public void findAll() {
+
+        // 1.读取mybatis的核心配置文件mybatis-config.xml
+
+        // 2.基于配置信息获取一个SqlSessionFactory工厂对象
+
+        // 3.通过工厂独享获取一个SqlSession对象
+
+        // 4.执行SQL语句(EmpMapper.xml),接收处理后的结果
+
+        // 5.输出结果
+
+    }
+
+}
